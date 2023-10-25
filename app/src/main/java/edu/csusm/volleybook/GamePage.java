64f0c9bbf23d322b4cac8c5af43caf935d388ffe
daelynpedroza <pedro043@csusm.edu>
2023-10-25 16:06:25 -0700

    Score incrementer
    added ability to increase the score on gamepage

--- app/src/main/java/edu/csusm/volleybook/GamePage.java
+++ app/src/main/java/edu/csusm/volleybook/GamePage.java
@@ -5,13 +5,33 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class GamePage extends AppCompatActivity {
+
+    private int count = 0;
+    private TextView valueTextView;
+    private Button incrementButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game_page);
+
+
+        valueTextView = findViewById(R.id.scoreA);
+        incrementButton = findViewById(R.id.plus_button1);
+
+        incrementButton.setOnClickListener(v -> {
+            // You can't change the value of 'count' as it's declared as final
+            count++; // This will result in a compilation error
+
+            // You can use 'count' in your logic
+            valueTextView.setText(String.valueOf(count));
+
+        });
+
     }
 
     public void TeamA (View v){
@@ -27,6 +47,7 @@ public class GamePage extends AppCompatActivity {
         Intent i = new Intent (this,TeamB.class);
         startActivity(i);
     }
+
 
 
 }
